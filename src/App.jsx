f26dd6234aaa0f84104e032a5d54281091c0fded
satsuki-yabu <67957382+satsuki-yabu@users.noreply.github.com>
2020-10-21 15:31:10 +0900

    HTML構成書いた

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,28 @@
 import React from "react";
 import "./styles.css";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <input placeholder="TODO入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了リスト</p>
+        <ul>
+          <li>TODOです</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+      </div>
+      <div>
+        <p>完了リスト</p>
+        <ul>
+          <li>TODOでした</li>
+          <button>戻す</button>
+        </ul>
+      </div>
+    </>
+  );
+};
