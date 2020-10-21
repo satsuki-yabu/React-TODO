7e1365a54b2e6fd46463a4caac2e89af1a89b471
satsuki-yabu <67957382+satsuki-yabu@users.noreply.github.com>
2020-10-21 15:37:10 +0900

    css適用（ここからReact学習開始！！）

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODO入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了リスト</p>
-        <ul>
+      <div className="incomplete-area">
+        <p className="title">未完了リスト</p>
+        <ul className="list-row">
           <li>TODOです</li>
           <button>完了</button>
           <button>削除</button>
         </ul>
       </div>
-      <div>
-        <p>完了リスト</p>
-        <ul>
+      <div className="complete-area">
+        <p className="title">完了リスト</p>
+        <ul className="list-row">
           <li>TODOでした</li>
           <button>戻す</button>
         </ul>
